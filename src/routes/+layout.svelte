67a84646032c5a4aba3ab87e98e61f28a99200fb
Josh Mosier <josh.r.mosier@gmail.com>
2025-02-14 12:28:24 -0500

    basic favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,4 +5,12 @@
   injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
+<svelte:head>
+  <title>Lactation Success | Professional Breastfeeding Support</title>
+  <meta name="description" content="Professional in-home and virtual breastfeeding support by Carolyn Mosier APRN, IBCLC, RLC" />
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+  <link rel="icon" href="/favicon.ico" sizes="any" />
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+</svelte:head>
+
 <slot /> 
