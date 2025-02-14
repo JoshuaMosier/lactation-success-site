a4a28ac48ef24898b6a1efe7cad81cfa3e9a95ea
Josh Mosier <josh.r.mosier@gmail.com>
2025-02-14 12:03:49 -0500

    analytics + speed insights

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { inject } from '@vercel/analytics';
+  import { SpeedInsights } from '@vercel/speed-insights/svelte';
+  
+  // Initialize analytics
+  inject();
+</script>
+
+<slot />
+<SpeedInsights /> 
